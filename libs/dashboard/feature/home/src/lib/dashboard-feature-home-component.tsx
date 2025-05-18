66f134ded2dff244ccd52568840ecb93a22495cf
NP <rezakiani05555@gmail.com>
2025-05-18 17:22:45 +0330

    fix: working on responsive dashboard

--- libs/dashboard/feature/home/src/lib/dashboard-feature-home-component.tsx
+++ libs/dashboard/feature/home/src/lib/dashboard-feature-home-component.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
-
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -22,19 +20,18 @@ import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 import { LineChart } from '@mui/x-charts/LineChart';
 import MailIcon from '@mui/icons-material/Mail';
 import PersonIcon from '@mui/icons-material/Person';
-
 import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
-
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-
 import ListItemIcon from '@mui/material/ListItemIcon';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-
 import DashboardFeatureGridComponent from './dashboard-feature-grid-component';
+import AppBar from '@mui/material/AppBar';
+import Drawer from '@mui/material/Drawer';
+
+const drawerWidth = 240;
 
 const settings = {
   width: 200,
@@ -46,255 +43,85 @@ const sample = [1, 10, 30, 50, 70, 90, 100];
 
 const navItems = ['Home', 'About', 'Contact'];
 
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme),
-      },
-    },
-    {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme),
-      },
-    },
-  ],
-}));
-
 export function DashboardFeatureHome() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}
-        open={open}
-      >
-        <Toolbar
-          style={{ backgroundColor: '#E2E8F0' }}
-          // sx={{
-          //   border: '1px solid blue',
-          // }}
-        >
+      <AppBar component="nav">
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
             edge="start"
-            sx={[
-              {
-                marginRight: 5,
-              },
-              open && { display: 'none' },
-            ]}
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
+          {/* <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography> */}
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {/* {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))} */}
+          </Box>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: 'initial',
-                      }
-                    : {
-                        justifyContent: 'center',
-                      },
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: 'center',
-                    },
-                    open
-                      ? {
-                          mr: 3,
-                        }
-                      : {
-                          mr: 'auto',
-                        },
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: 'initial',
-                      }
-                    : {
-                        justifyContent: 'center',
-                      },
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: 'center',
-                    },
-                    open
-                      ? {
-                          mr: 3,
-                        }
-                      : {
-                          mr: 'auto',
-                        },
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
       <Box component="main" className="flex-1 flex flex-col p-3  ">
         <Toolbar />
-        <div className=" flex-1 mx-24 justify-items-center ">
-          <div className="grid grid-cols-4 w-full gap-4">
-            <div className="col-span-3 grid grid-cols-3 gap-4 ">
+        <div className=" flex-1 lg:mx-24 justify-items-center ">
+          <div className="grid grid-cols-1   sm:grid-cols-2 lg:grid-cols-4 w-full gap-4">
+            <div className="col-span-1 sm:col-span-2    grid grid-cols-1  sm:grid-cols-2   gap-4 ">
               <DashboardFeatureCardsComponent
                 icon={
                   <MailIcon
@@ -309,6 +136,8 @@ export function DashboardFeatureHome() {
                   />
                 }
               />
+            </div>
+            <div className="col-span-1 sm:col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
               <DashboardFeatureCardsComponent
                 icon={
                   <MailIcon
@@ -316,8 +145,6 @@ export function DashboardFeatureHome() {
                   />
                 }
               />
-            </div>
-            <div className="col-span-1 grid grid-cols-1 gap-4">
               <DashboardFeatureCardsComponent
                 icon={
                   <MailIcon
@@ -327,8 +154,41 @@ export function DashboardFeatureHome() {
               />
             </div>
           </div>
-          <div className="grid grid-cols-4 w-full mt-12 gap-4">
-            <div className="col-span-2 grid grid-cols-2 gap-4 ">
+
+          <div className="grid grid-cols-1  md:grid-cols-4 w-full mt-12 gap-4">
+            <div className="grid  col-span-1 md:col-span-3">
+              <Card className="col-span-3 " style={{ borderRadius: '30px' }}>
+                <DashboardFeatureGridComponent />
+              </Card>
+            </div>
+            <div className="col-span-1 grid grid-cols-1 gap-4">
+              <Card
+                className="col-span-1 flex justify-center  w-full"
+                style={{ borderRadius: '30px' }}
+              >
+                <PieChart
+                  series={[
+                    {
+                      data: desktopOS,
+                      highlightScope: { fade: 'global', highlight: 'item' },
+                      faded: {
+                        innerRadius: 30,
+                        additionalRadius: -30,
+                        color: 'gray',
+                      },
+                      valueFormatter,
+                    },
+                  ]}
+                  hideLegend
+                  height={200}
+                  // width={200}
+                />
+              </Card>
+            </div>
+          </div>
+
+          <div className="grid  grid-cols-1  md:grid-cols-4 w-full mt-12 gap-4">
+            <div className="col-span-2  grid grid-cols-2  gap-4 ">
               <Card
                 className="col-span-3 h-[310px]  "
                 style={{ borderRadius: '30px' }}
@@ -362,112 +222,6 @@ export function DashboardFeatureHome() {
               </Card>
             </div>
           </div>
-          <div className="grid grid-cols-4 w-full mt-12 gap-4">
-            <div className="col-span-3 grid grid-cols-3 gap-4 ">
-              <Card
-                className="col-span-3 h-[310px]  "
-                style={{ borderRadius: '30px' }}
-              >
-                <DashboardFeatureGridComponent />
-              </Card>
-            </div>
-            <div className="col-span-1 grid grid-cols-1 gap-4">
-              <Card
-                className="col-span-1 flex justify-center"
-                style={{ borderRadius: '30px' }}
-              >
-                <PieChart
-                  series={[
-                    {
-                      data: desktopOS,
-                      highlightScope: { fade: 'global', highlight: 'item' },
-                      faded: {
-                        innerRadius: 30,
-                        additionalRadius: -30,
-                        color: 'gray',
-                      },
-                      valueFormatter,
-                    },
-                  ]}
-                  hideLegend
-                  height={200}
-                  width={200}
-                />
-              </Card>
-            </div>
-          </div>
-          {/* <div className=" grid grid-cols-4  mt-12 gap-4">
-             <div className="col-span-3 grid grid-cols-3">
-              <Card style={{ borderRadius: '30px' }}>
-                <DashboardFeatureGridComponent />
-              </Card>
-            </div> 
-            <div>
-              <Card
-                style={{ borderRadius: '30px' }}
-                className="flex justify-center items-center"
-              >
-                <PieChart
-                  series={[
-                    {
-                      data: desktopOS,
-                      highlightScope: { fade: 'global', highlight: 'item' },
-                      faded: {
-                        innerRadius: 30,
-                        additionalRadius: -30,
-                        color: 'gray',
-                      },
-                      valueFormatter,
-                    },
-                  ]}
-                  height={200}
-                  width={200}
-                />
-              </Card>
-            </div>
-            <div className="col-span-1 grid grid-cols-1 gap-4 ">
-              <Card
-                style={{ borderRadius: '30px' }}
-                className="flex justify-center items-center"
-              >
-                <Gauge
-                  {...settings}
-                  cornerRadius="50%"
-                  sx={(theme) => ({
-                    [`& .${gaugeClasses.valueText}`]: {
-                      fontSize: 40,
-                    },
-                    [`& .${gaugeClasses.valueArc}`]: {
-                      fill: '#52b202',
-                    },
-                    [`& .${gaugeClasses.referenceArc}`]: {
-                      fill: theme.palette.text.disabled,
-                    },
-                  })}
-                />
-              </Card>
-            </div>
-            <div className="col-span-3 grid grid-cols-3 gap-4 ">
-              <Card
-                className="col-span-3 h-[310px]  "
-                style={{ borderRadius: '30px' }}
-              >
-                <LineChart
-                  xAxis={[{ data: sample }]}
-                  yAxis={[
-                    { id: 'linearAxis', scaleType: 'linear', position: 'left' },
-                    { id: 'logAxis', scaleType: 'log', position: 'right' },
-                  ]}
-                  series={[
-                    { yAxisId: 'linearAxis', data: sample, label: 'linear' },
-                    { yAxisId: 'logAxis', data: sample, label: 'log' },
-                  ]}
-                  height={300}
-                  // width={1000}
-                />
-              </Card>
-            </div>
-          </div> */}
         </div>
       </Box>
     </Box>
