d99c4c343f830ecdd5527c37c7d2af2fe7d9568e
NP <rezakiani05555@gmail.com>
2025-05-22 08:51:54 +0330

    fix: add font in app workflow-engine

--- apps/workflow-engine/src/app/app.tsx
+++ apps/workflow-engine/src/app/app.tsx
@@ -3,8 +3,11 @@ import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
 import rtlPlugin from 'stylis-plugin-rtl';
-// import { WorkFlowFeatureShell } from '@office-automation/workflow-engine/feature/shell';
+
 import { WorkflowEngineFeatureWorkflowEngineShell } from '@office-automation/workflow-engine/feature/workflow-engine-shell';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme } from '@office-automation/shared/util/mui-theme';
 
 const rtlCache = createCache({
   key: 'muirtl',
@@ -28,9 +31,12 @@ const router = createBrowserRouter(
 export function App() {
   return (
     <div>
-      <CacheProvider value={rtlCache}>
-        <RouterProvider router={router} />
-      </CacheProvider>
+      <ThemeProvider theme={theme}>
+        <CacheProvider value={rtlCache}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </CacheProvider>
+      </ThemeProvider>
     </div>
   );
 }
--- libs/workflow-engine/feature/workflow-engine-shell/src/lib/workflow-engine-feature-workflow-engine-shell.tsx
+++ libs/workflow-engine/feature/workflow-engine-shell/src/lib/workflow-engine-feature-workflow-engine-shell.tsx
@@ -3,7 +3,34 @@ import { SharedUiDrawer } from '@office-automation/shared/ui/drawer';
 export function WorkflowEngineFeatureWorkflowEngineShell() {
   return (
     <div>
-      <SharedUiDrawer />
+      <SharedUiDrawer
+        menu={[
+          {
+            id: 346,
+            route: '',
+            title: 'درخواست کالا',
+            shouldCreateTable: false,
+          },
+          {
+            id: 405,
+            route: '',
+            title: 'ورود کالا به کارخانه',
+            shouldCreateTable: false,
+          },
+          {
+            id: 406,
+            route: '',
+            title: 'خروج کالا از کارخانه',
+            shouldCreateTable: false,
+          },
+          {
+            id: 407,
+            route: '',
+            title: 'پیش فاکتور',
+            shouldCreateTable: false,
+          },
+        ]}
+      />
     </div>
   );
 }
