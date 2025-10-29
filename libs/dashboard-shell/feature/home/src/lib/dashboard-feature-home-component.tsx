4fb8167e9cc70bef708d82e1fe2bee31798e3013
reza <rezakiani05555@gmail.com>
2025-10-30 00:15:56 +0330

    fix: ad package for backgroud dashboard-shell

--- libs/dashboard-shell/feature/home/src/lib/dashboard-feature-home-component.tsx
+++ libs/dashboard-shell/feature/home/src/lib/dashboard-feature-home-component.tsx
@@ -23,6 +23,12 @@ import AppBar from '@mui/material/AppBar';
 import Drawer from '@mui/material/Drawer';
 import { UserProfileMenu } from './components/UserProfileMenu';
 import { NotificationMenu } from './components/NotificationMenu';
+import { useCallback, useEffect, useState } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 const drawerWidth = 240;
 
@@ -37,6 +43,27 @@ export function DashboardFeatureHome() {
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
+  };
+
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
   };
 
   // State for mobile drawer
@@ -66,7 +93,13 @@ export function DashboardFeatureHome() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar component="nav">
+      {/* Use MUI props to make AppBar transparent and remove shadow */}
+      <AppBar
+        component="nav"
+        color="transparent"
+        elevation={0}
+        sx={{ bgcolor: 'transparent' }}
+      >
         <Toolbar className="flex justify-between">
           {/* <IconButton
             color="inherit"
@@ -156,8 +189,84 @@ export function DashboardFeatureHome() {
         </Drawer>
       </nav>
       <Box component="main" className="flex-1 flex flex-col p-3  ">
+        {init && (
+          <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+              background: {
+                color: {
+                  value: '#3b3e46ff',
+                },
+              },
+              fpsLimit: 3000,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: false,
+                    // mode: '',
+                  },
+                  onHover: {
+                    enable: false,
+                    // mode: 'repulse',
+                  },
+                  resize: true,
+                },
+                modes: {
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 0,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: '#ffffff',
+                },
+                links: {
+                  color: '#ffffff',
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                move: {
+                  direction: 'none',
+                  enable: true,
+                  outModes: {
+                    default: 'bounce',
+                  },
+                  random: false,
+                  speed: 2,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 800,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: 'circle',
+                },
+                size: {
+                  value: { min: 1, max: 5 },
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        )}
+
         <Toolbar />
-        <div className=" flex-1 lg:mx-24 justify-items-center ">
+        <div className=" flex-1 lg:mx-24 justify-items-center  ">
           <div className="grid grid-cols-1   sm:grid-cols-2 lg:grid-cols-4 w-full gap-4">
             <div className="col-span-1 sm:col-span-2    grid grid-cols-1  sm:grid-cols-2   gap-4 ">
               <DashboardFeatureCardsComponent
